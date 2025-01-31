baeab63121ec611d7558510ac97f62a9827721d6
Yuskus <yuskus@yandex.ru>
2025-01-31 22:38:25 +0300

    Forgotten Line

--- OnlineStore.Server/OnlineStore.Server/Repositories/Order/IOrderRepository.cs
+++ OnlineStore.Server/OnlineStore.Server/Repositories/Order/IOrderRepository.cs
@@ -8,7 +8,6 @@ namespace OnlineStore.Server.Repositories.Order
         Task<IEnumerable<OrderResponse>> GetPageOfOrdersByCustomerId(Guid id, int pageNumber, int pageSize); //add count() to response
         Task<IEnumerable<OrderResponse>> GetPageOfOrdersByStatus(string status, int pageNumber, int pageSize); //add count() to response
         Task<OrderResponse?> GetOrderByNumber(int number);
-        Task<OrderResponse?> GetBasketByCustomerId(Guid id);
         Task<Guid?> CreateOrder(OrderRequest order);
         Task<bool> UpdateOrder(Guid id, OrderRequest order);
         Task<bool> DeleteOrder(Guid id);
